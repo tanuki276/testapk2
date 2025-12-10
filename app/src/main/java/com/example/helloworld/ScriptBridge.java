c53c6aca3a212cf0b8f802d9500c90ec5f07c6dc
blowa <light06120602@gmail.com>
2025-12-10 21:50:49 +0900

    ScriptBridge.java の更新

--- app/src/main/java/com/example/helloworld/ScriptBridge.java
+++ app/src/main/java/com/example/helloworld/ScriptBridge.java
@@ -1,30 +1,35 @@
-package com.ghosthacker.app;
+package com.tci.injector;
 
 import android.content.Context;
 import android.webkit.JavascriptInterface;
 import android.widget.Toast;
+import android.util.Log;
 
-public class ScriptBridge {
+public class TCIBridge {
+    
     private Context context;
+    private static final String TAG = "TCI_JS_ENGINE";
 
-    public ScriptBridge(Context c) {
+    public TCIBridge(Context c) {
         this.context = c;
     }
 
     @JavascriptInterface
     public void log(String message) {
-        
+        Log.d(TAG, "[JS_LOG] " + message);
     }
 
     @JavascriptInterface
     public void showToast(String message) {
-        Toast.makeText(context, "JS CALL: " + message, Toast.LENGTH_SHORT).show();
+        Toast.makeText(context, 
+            "TCI STATUS: " + message, 
+            Toast.LENGTH_SHORT).show();
     }
 
     @JavascriptInterface
     public void setGameValue(String targetAddress, int newValue) {
         Toast.makeText(context, 
-            "Native Hook: " + targetAddress + " <-- " + newValue + " (SUCCESS)", 
+            "INJECT SUCCESS: Address " + targetAddress + " overwritten with " + newValue, 
             Toast.LENGTH_LONG).show();
     }
 }
