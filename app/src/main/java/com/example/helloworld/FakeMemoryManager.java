d4ca52b586a079fd5aed07908ede5c6317266cd2
blowa <light06120602@gmail.com>
2025-12-10 21:56:13 +0900

    FakeMemoryManager.java の更新

--- app/src/main/java/com/example/helloworld/FakeMemoryManager.java
+++ app/src/main/java/com/example/helloworld/FakeMemoryManager.java
@@ -1,4 +1,4 @@
-package com.ghosthacker.app;
+package com.tci.injector;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -7,8 +7,10 @@ import java.util.Random;
 
 public class FakeMemoryManager {
     private static List<MemoryResult> currentResults = new ArrayList<>();
-    
+
     private static FakeMemoryManager instance;
+    private final String[] MEMORY_REGIONS = {"Heap", "Stack", "Code", "Anon", "RWX", "AIB"};
+    private final Random rand = new Random();
 
     public static FakeMemoryManager getInstance() {
         if (instance == null) {
@@ -19,23 +21,33 @@ public class FakeMemoryManager {
 
     public static class MemoryResult {
         public String fakeAddress; 
-        public int valueDecimal;   
+        public String region;      
+        public int valueDec;       
         public String valueHex;    
         public boolean isFrozen = false;
 
-        public MemoryResult(String address, int decValue) {
+        public MemoryResult(String address, String region, int decValue) {
             this.fakeAddress = address;
-            this.valueDecimal = decValue;
+            this.region = region;
+            this.valueDec = decValue;
             this.valueHex = String.format(Locale.US, "0x%08X", decValue); 
         }
 
         public void setValue(int newValue) {
-            this.valueDecimal = newValue;
+            this.valueDec = newValue;
             this.valueHex = String.format(Locale.US, "0x%08X", newValue);
         }
     }
 
     public List<MemoryResult> getCurrentResults() {
+        // GG風: Freezeされていない値をランダムに変動させる
+        for (MemoryResult result : currentResults) {
+            if (!result.isFrozen && rand.nextDouble() < 0.3) {
+                // 30%の確率で±1変動させることで、Refineの必要性をシミュレート
+                int change = rand.nextBoolean() ? 1 : -1;
+                result.setValue(result.valueDec + change);
+            }
+        }
         return currentResults;
     }
 
@@ -45,27 +57,35 @@ public class FakeMemoryManager {
 
     public void generateNewResults(int searchValue) {
         clearResults();
-        Random rand = new Random();
-        int totalEntries = rand.nextInt(400) + 100;
+        
+        // GG風: 検索数に幅を持たせる
+        int totalEntries = rand.nextInt(500) + 150; 
 
         for (int i = 0; i < totalEntries; i++) {
-            String address = generateFakeAddress(rand);
+            String address = generateFakeAddress();
+            String region = MEMORY_REGIONS[rand.nextInt(MEMORY_REGIONS.length)];
             int value;
-            
-            if (i < totalEntries * 0.05) {
+
+            // 検索値と完全に一致するものを少なくする
+            if (i < totalEntries * 0.02) { 
                 value = searchValue;
-            } else if (i < totalEntries * 0.15) {
-                value = searchValue + rand.nextInt(21) - 10;
-            } else {
+            } 
+            // 検索値の近傍の値を多くする (ファジー検索のシミュレーション)
+            else if (i < totalEntries * 0.1) { 
+                value = searchValue + rand.nextInt(31) - 15;
+            } 
+            // ノイズとしてランダムな値を格納
+            else {
                 value = rand.nextInt(1000000); 
             }
-            
-            currentResults.add(new MemoryResult(address, value));
+
+            currentResults.add(new MemoryResult(address, region, value));
         }
     }
-    
-    private String generateFakeAddress(Random rand) {
-        long randomAddr = (long) (rand.nextDouble() * 0x1FFFFFFFFL) + 0x700000000L; 
-        return String.format(Locale.US, "0x%010X (RWX)", randomAddr); 
+
+    private String generateFakeAddress() {
+        // GG風: よりリアルなAndroidの64ビットアドレス空間をシミュレート (0x7...から始まる)
+        long randomAddr = (long) (rand.nextDouble() * 0x1FFFFFFFFL) + 0x7000000000L; 
+        return String.format(Locale.US, "0x%012X", randomAddr); // 12桁のHexアドレス
     }
 }
